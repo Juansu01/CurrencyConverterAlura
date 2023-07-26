4eb52c54efba81b177c0584e216dacc1a6d00dc5
Juansu01 <thejuank0@hotmail.com>
2023-07-26 13:05:05 -0500

    Add user input validation

--- src/main/java/com/example/currencyconverteralura/CurrencyConverterController.java
+++ src/main/java/com/example/currencyconverteralura/CurrencyConverterController.java
@@ -5,6 +5,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,6 +19,12 @@ public class CurrencyConverterController implements Initializable {
 
     @FXML
     public ChoiceBox<String> haveChoiceBox = new ChoiceBox<String>();
+
+    @FXML
+    public Text haveError = new Text();
+
+    @FXML
+    public Text wantError = new Text();
     @FXML
     public ChoiceBox<String> wantChoiceBox = new ChoiceBox<String>();
 
@@ -27,7 +37,35 @@ public class CurrencyConverterController implements Initializable {
         wantChoiceBox.getItems().addAll(availableCurrencies);
     }
 
-    public void getCurrencies(ActionEvent event) {
+    public void onSubmit(ActionEvent event) {
+        String haveValue = haveChoiceBox.getValue();
+        String wantValue = wantChoiceBox.getValue();
+        boolean isValid = validateInput();
+        if (isValid) {
+            System.out.println("The user wants:" + haveValue + " " + wantValue);
+        }
 
+    }
+
+    public boolean validateInput() {
+        String haveVal = haveChoiceBox.getValue();
+        String wantVal = wantChoiceBox.getValue();
+
+        if (haveVal == null) {
+            haveError.setText("Select option");
+        } else {
+            haveError.setText("");
+        }
+
+        if (wantVal == null) {
+            wantError.setText("Select option");
+        } else {
+            wantError.setText("");
+        }
+        if (haveVal == null || wantVal == null) {
+            return false;
+        }
+
+        return true;
     }
 }
