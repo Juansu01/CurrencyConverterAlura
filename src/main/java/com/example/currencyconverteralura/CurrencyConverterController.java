7afc7a04ae140d17b13f5e5bd8344da047e77954
Juansu01 <thejuank0@hotmail.com>
2023-07-26 11:46:16 -0500

    Add Label content

--- src/main/java/com/example/currencyconverteralura/CurrencyConverterController.java
+++ src/main/java/com/example/currencyconverteralura/CurrencyConverterController.java
@@ -1,14 +1,33 @@
 package com.example.currencyconverteralura;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
-public class CurrencyConverterController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CurrencyConverterController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private Label welcomeText = new Label("Hello?");
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public ChoiceBox<String> haveChoiceBox = new ChoiceBox<String>();
+    @FXML
+    public ChoiceBox<String> wantChoiceBox = new ChoiceBox<String>();
+
+    private String[] availableCurrencies = {"COP", "USD", "BRL", "EUR", "CAD", "AUD", "MXN"};
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        welcomeText.setText("Select currencies");
+        haveChoiceBox.getItems().addAll(availableCurrencies);
+        wantChoiceBox.getItems().addAll(availableCurrencies);
+    }
+
+    public void getCurrencies(ActionEvent event) {
+
     }
 }
