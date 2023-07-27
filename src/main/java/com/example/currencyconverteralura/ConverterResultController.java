82bd5d53180f7db6120f4a8ed22b173dbf00891e
Juansu01 <thejuank0@hotmail.com>
2023-07-27 12:01:23 -0500

    Add converted result fxml file and class

---
+++ src/main/java/com/example/currencyconverteralura/ConverterResultController.java
@@ -0,0 +1,12 @@
+package com.example.currencyconverteralura;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+
+public class ConverterResultController {
+}
