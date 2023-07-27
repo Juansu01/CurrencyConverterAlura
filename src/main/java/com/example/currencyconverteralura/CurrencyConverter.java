201c5214634056bc6e3e5f8090dc5bc178720687
Juansu01 <thejuank0@hotmail.com>
2023-07-27 11:50:04 -0500

    Added icon to application

--- src/main/java/com/example/currencyconverteralura/CurrencyConverter.java
+++ src/main/java/com/example/currencyconverteralura/CurrencyConverter.java
@@ -3,6 +3,7 @@ package com.example.currencyconverteralura;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,11 @@ import java.io.IOException;
 public class CurrencyConverter extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CurrencyConverter.class.getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CurrencyConverter.class.getResource("converter-prompt.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Alura Currency Converter");
+        Image icon = new Image("icon.png");
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
     }
