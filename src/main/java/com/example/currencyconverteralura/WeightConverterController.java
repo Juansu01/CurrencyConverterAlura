800fb0d309be963e584dbe22f363b6323eb1ea0c
Juansu01 <thejuank0@hotmail.com>
2023-07-30 14:03:31 -0500

    Add go back to home page functionality

--- src/main/java/com/example/currencyconverteralura/WeightConverterController.java
+++ src/main/java/com/example/currencyconverteralura/WeightConverterController.java
@@ -2,11 +2,17 @@ package com.example.currencyconverteralura;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -35,6 +41,14 @@ public class WeightConverterController implements Initializable {
 
     public final double ratio = 0.45359237;
 
+    private FXMLLoader homeLoader = new FXMLLoader(getClass().getResource("alura-converter-home.fxml"));
+
+    private Stage stage;
+
+    private Scene scene;
+
+    private Parent root;
+
 
     private String[] availableUnits = {"Kilos", "Pounds"};
 
@@ -51,6 +65,11 @@ public class WeightConverterController implements Initializable {
             double result = this.getResult();
             this.result.setText(String.valueOf(result));
         }
+    }
+
+    public void goHome(ActionEvent event) throws IOException {
+        this.root = this.homeLoader.load();
+        this.displayWindow(event);
     }
 
     public boolean validateInput() {
@@ -118,5 +137,12 @@ public class WeightConverterController implements Initializable {
 
     public double convertPoundsToKilos(double pounds) {
         return pounds * this.ratio;
+    }
+
+    public void displayWindow(ActionEvent event) {
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
