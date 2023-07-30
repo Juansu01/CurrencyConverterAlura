d1815a45268ab0437da5ec50d630561a18bc980f
Juansu01 <thejuank0@hotmail.com>
2023-07-30 12:34:53 -0500

    Add home controller with choice validation

---
+++ src/main/java/com/example/currencyconverteralura/HomeController.java
@@ -0,0 +1,49 @@
+package com.example.currencyconverteralura;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
+
+    @FXML
+    private ChoiceBox<String> converterChoiceBox = new ChoiceBox<String>();
+
+    private String[] availableConverters = {"Currency", "Weight"};
+
+    @FXML
+    public Text converterChoiceError = new Text();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.converterChoiceError.setText("");
+        this.setConverterChoiceBox();
+    }
+
+    public void onHomeSubmit(ActionEvent event) {
+        boolean isChoiceValid = this.validateChoice();
+        if (isChoiceValid) {
+            System.out.println("Valid choice.");
+        }
+    }
+
+    public void setConverterChoiceBox() {
+        this.converterChoiceBox.getItems().addAll(this.availableConverters);
+    }
+
+    public boolean validateChoice() {
+        boolean isChoiceNull = this.converterChoiceBox.getValue() == null;
+        if (isChoiceNull) {
+            this.converterChoiceError.setText("Please pick a converter");
+        } else {
+            this.converterChoiceError.setText("");
+        }
+
+        return !isChoiceNull;
+    }
+}
